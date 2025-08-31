0697aa19807ccb037276905339b75350194b7b9a
Hazy <164863088+Hazy0189@users.noreply.github.com>
2025-08-31 18:52:25 +0700

    Add solve script on TFC CTF

---
+++ CTFTime/2025/TFC CTF 2025/README.md
@@ -0,0 +1,5 @@
+# TFC CTF 2025
+
+| Challenge | Category | Tag | Description | 
+| --- | :---: | :---: | --- |
+| mucusky | pwn | `stack` `em_csky` `ret2shellcode` | the stack address in binary in fix and also rwx so we can just first leak stack then input our shellcode and call the stack address |
---
+++ CTFTime/2025/TFC CTF 2025/mucusky/mucusuki.c
@@ -0,0 +1,212 @@
+typedef unsigned char   undefined;
+
+typedef unsigned char    byte;
+typedef unsigned int    dword;
+typedef unsigned int    undefined4;
+typedef unsigned short    word;
+typedef enum Elf_ProgramHeaderType {
+    PT_NULL=0,
+    PT_LOAD=1,
+    PT_DYNAMIC=2,
+    PT_INTERP=3,
+    PT_NOTE=4,
+    PT_SHLIB=5,
+    PT_PHDR=6,
+    PT_TLS=7,
+    PT_GNU_EH_FRAME=1685382480,
+    PT_GNU_STACK=1685382481,
+    PT_GNU_RELRO=1685382482
+} Elf_ProgramHeaderType;
+
+typedef struct Elf32_Phdr Elf32_Phdr, *PElf32_Phdr;
+
+struct Elf32_Phdr {
+    enum Elf_ProgramHeaderType p_type;
+    dword p_offset;
+    dword p_vaddr;
+    dword p_paddr;
+    dword p_filesz;
+    dword p_memsz;
+    dword p_flags;
+    dword p_align;
+};
+
+typedef struct Elf32_Shdr Elf32_Shdr, *PElf32_Shdr;
+
+typedef enum Elf_SectionHeaderType {
+    SHT_NULL=0,
+    SHT_PROGBITS=1,
+    SHT_SYMTAB=2,
+    SHT_STRTAB=3,
+    SHT_RELA=4,
+    SHT_HASH=5,
+    SHT_DYNAMIC=6,
+    SHT_NOTE=7,
+    SHT_NOBITS=8,
+    SHT_REL=9,
+    SHT_SHLIB=10,
+    SHT_DYNSYM=11,
+    SHT_INIT_ARRAY=14,
+    SHT_FINI_ARRAY=15,
+    SHT_PREINIT_ARRAY=16,
+    SHT_GROUP=17,
+    SHT_SYMTAB_SHNDX=18,
+    SHT_ANDROID_REL=1610612737,
+    SHT_ANDROID_RELA=1610612738,
+    SHT_GNU_ATTRIBUTES=1879048181,
+    SHT_GNU_HASH=1879048182,
+    SHT_GNU_LIBLIST=1879048183,
+    SHT_CHECKSUM=1879048184,
+    SHT_SUNW_move=1879048186,
+    SHT_SUNW_COMDAT=1879048187,
+    SHT_SUNW_syminfo=1879048188,
+    SHT_GNU_verdef=1879048189,
+    SHT_GNU_verneed=1879048190,
+    SHT_GNU_versym=1879048191
+} Elf_SectionHeaderType;
+
+struct Elf32_Shdr {
+    dword sh_name;
+    enum Elf_SectionHeaderType sh_type;
+    dword sh_flags;
+    dword sh_addr;
+    dword sh_offset;
+    dword sh_size;
+    dword sh_link;
+    dword sh_info;
+    dword sh_addralign;
+    dword sh_entsize;
+};
+
+typedef struct Elf32_Ehdr Elf32_Ehdr, *PElf32_Ehdr;
+
+struct Elf32_Ehdr {
+    byte e_ident_magic_num;
+    char e_ident_magic_str[3];
+    byte e_ident_class;
+    byte e_ident_data;
+    byte e_ident_version;
+    byte e_ident_osabi;
+    byte e_ident_abiversion;
+    byte e_ident_pad[7];
+    word e_type;
+    word e_machine;
+    dword e_version;
+    dword e_entry;
+    dword e_phoff;
+    dword e_shoff;
+    dword e_flags;
+    word e_ehsize;
+    word e_phentsize;
+    word e_phnum;
+    word e_shentsize;
+    word e_shnum;
+    word e_shstrndx;
+};
+
+
+
+
+undefined4 FUN_00008150(void)
+
+{
+  undefined4 uVar1;
+  undefined auStack_6c [100];
+  
+  write(1,"Give me something to read:\n",0x1b);
+  uVar1 = read(0,auStack_6c,0x100);
+  return uVar1;
+}
+
+
+
+undefined4 entry(void)
+
+{
+  undefined4 uVar1;
+  
+  FUN_00008150();
+  printf("Goodbye!\n");
+  uVar1 = FUN_0000832c(0);
+  return uVar1;
+}
+
+
+
+undefined4 FUN_00008208(undefined4 param_1,undefined4 param_2)
+
+{
+  trap_exception(0);
+  return param_2;
+}
+
+
+
+undefined4 FUN_00008264(undefined4 param_1,undefined4 param_2,undefined4 param_3)
+
+{
+  undefined4 uVar1;
+  
+  trap_exception(0);
+  uVar1 = FUN_00008208(0x54,param_2,param_2,param_3);
+  return uVar1;
+}
+
+
+
+undefined4 read(undefined4 param_1,undefined4 param_2,undefined4 param_3)
+
+{
+  undefined4 uVar1;
+  
+  uVar1 = FUN_00008208(0x54,param_1,param_2,param_3);
+  return uVar1;
+}
+
+
+
+undefined4 write(undefined4 param_1,undefined4 param_2,undefined4 param_3)
+
+{
+  undefined4 uVar1;
+  
+  uVar1 = FUN_00008208(0x55,param_1,param_2,param_3);
+  return uVar1;
+}
+
+
+
+undefined4 FUN_0000832c(undefined4 param_1)
+
+{
+  undefined4 uVar1;
+  
+  uVar1 = FUN_00008264(0x72,param_1);
+  return uVar1;
+}
+
+
+
+int FUN_00008360(int param_1)
+
+{
+  int local_8;
+  
+  for (local_8 = 0; *(char *)(local_8 + param_1) != '\0'; local_8 = local_8 + 1) {
+  }
+  return local_8;
+}
+
+
+
+undefined4 printf(undefined4 param_1)
+
+{
+  undefined4 uVar1;
+  
+  uVar1 = FUN_00008360(param_1);
+  uVar1 = write(1,param_1,uVar1);
+  return uVar1;
+}
+
+
